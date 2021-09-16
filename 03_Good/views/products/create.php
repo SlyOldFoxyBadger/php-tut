4a6b30c36480359be03b3abaaf1b581bacda38d8
SlyOldFoxyBadger <44817280+SlyOldFoxyBadger@users.noreply.github.com>
2021-09-16 15:50:31 +0200

    New files and funcs 2

--- 03_Good/views/products/create.php
+++ 03_Good/views/products/create.php
@@ -1,8 +1,38 @@
-<h1>Create Product</h1>
-<?php
-echo '<pre>';
-var_dump($products);
-echo '</pre>';
+<?php if (!empty($errors)): ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error): ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+<?php endif; ?>
 
+<form action="" method="POST" enctype="multipart/form-data">
 
-?>
+    <?php if ($product['image']): ?>
+        <img class="update-image" src="/<?php echo $product['image']; ?>" alt="">
+    <?php endif; ?>
+
+    <div class="mb-3">
+        <label>Product Image</label>
+        <br>
+        <input name="image" type="file">
+    </div>
+
+    <div class="mb-3">
+        <label>Product title</label>
+        <input name="title" type="text" class="form-control" value="<?php echo $product['title'] ?>">
+    </div>
+
+    <div class="mb-3">
+        <label>Product description</label>
+        <textarea name="description" class="form-control"><?php echo $product['description'] ?></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label>Product price</label>
+        <input name="price" type="number" step=".01" class="form-control" value="<?php echo $product['price'] ?>">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+    
+</form>
